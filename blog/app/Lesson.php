e172841e3bf99d4c92634a4464fe28915b83bb77
Hubert Suprunowicz <hubertsuprunowicz@gmail.com>
2019-04-19 16:48:53 +0200

    lesson belongsTo User

--- blog/app/Lesson.php
+++ blog/app/Lesson.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\DB;
 
 class Lesson extends Model
 {
@@ -17,6 +18,23 @@ class Lesson extends Model
 	protected $fillable = [
 		'author_id', 'date', 'price', 'subject', 'length', 'student_limit', 'description'
 	];
+
+	public function lesson() {
+		return $this->belongsTo(User::class);
+	}
+
+	public function lessonPost($lesson) {
+		DB::table('users')->insert([
+			[
+				'date' => $lesson['date'],
+				'price' => $lesson['price'],
+				'subject' => $lesson['subject'],
+				'length' => $lesson['length'],
+				'student_limit' => $lesson['student_limit'],
+				'description' => $lesson['description']
+			]
+		]);
+	}
 
 
 }
